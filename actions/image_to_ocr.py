49843ea8438b5a9df875512d0b17f3e86e7dcde3
Vishnu_Anilkumar <vishnu.mi1@iiitmk.ac.in>
2023-10-25 22:08:23 +0530

    pil to cv2

--- actions/image_to_ocr.py
+++ actions/image_to_ocr.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import easyocr
 from EasyOcrImageToTextGradio.utils.helper import draw_boxes
@@ -6,7 +5,7 @@ from EasyOcrImageToTextGradio.utils.helper import draw_boxes
 
 def inference(img, lang):
     reader = easyocr.Reader(lang, gpu=True)
-    bounds = reader.readtext(np.array(img))
+    bounds = reader.readtext(img)
     im = img.copy()
     draw_boxes(im, bounds)
     return [im, pd.DataFrame(bounds, columns=['Coordinates', 'Text', 'Confidence']).iloc[:, 0:].astype(str)]
