746bb01cbe3785fb28caf89507378d9613502543
Vishnu_Anilkumar <vishnu.mi1@iiitmk.ac.in>
2023-10-25 13:06:03 +0530

    Add files via upload

---
+++ utils/constants.py
@@ -0,0 +1,12 @@
+title = 'WEB snippets to TEXT'
+
+choices = [
+    "ch_sim",
+    "ch_tra",
+    "de",
+    "en",
+    "es",
+    "ja",
+    "hi",
+    "ru"
+]
---
+++ utils/helper.py
@@ -0,0 +1,9 @@
+from PIL import ImageDraw
+
+
+def draw_boxes(image, bounds, color='yellow', width=2):
+    draw = ImageDraw.Draw(image)
+    for bound in bounds:
+        p0, p1, p2, p3 = bound[0]
+        draw.line([*p0, *p1, *p2, *p3, *p0], fill=color, width=width)
+    return image
