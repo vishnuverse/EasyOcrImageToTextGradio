abf8aa4744b8faf3d8d6cc98cc1e63491d4ebbe4
Vishnu_Anilkumar <vishnu.mi1@iiitmk.ac.in>
2023-10-25 22:08:53 +0530

    pil to cv2

--- utils/helper.py
+++ utils/helper.py
@@ -1,9 +1,13 @@
-from PIL import ImageDraw
+
+import cv2
+import numpy as np
 
 
-def draw_boxes(image, bounds, color='yellow', width=2):
-    draw = ImageDraw.Draw(image)
+def draw_boxes(image_copy, bounds, color=(0, 255, 0), thickness=5):
     for bound in bounds:
         p0, p1, p2, p3 = bound[0]
-        draw.line([*p0, *p1, *p2, *p3, *p0], fill=color, width=width)
-    return image
+        pts = np.array([p0, p1, p2, p3], np.int32)
+        pts = pts.reshape((-1, 1, 2))
+        image_copy = cv2.polylines(image_copy, [pts], isClosed=True, color=color, thickness=thickness)
+
+    return image_copy
